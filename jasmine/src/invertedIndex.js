5e566973bf2c0c81e4b663e55b979e5f380a2934
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-09-17 20:50:04 +0100

    refactor code to make it scalable

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,47 +1,67 @@
-function getBooks(filePath) {
-  var arrObject;
+// get the json file contents
+var getBooks = function() {
+  var booksJson;
   $.ajax({
     async: false,
-    url: './books.json',
+    url: 'books.json',
     dataType: 'json',
-    success: function(data) {
-      arrObject = data;
+    success: function(response) {
+      booksJson = response;
     }
   });
-  return arrObject;
-}
+  return booksJson;
+};
 var books = getBooks();
 
-function Index(){}
-
-var objZero = books[0].title + ' ' + books[0].text;
-var objOne = books[1].title + ' ' + books[1].text;
-var tokenZero = objZero.toLowerCase().replace('.', '').split(' ');
-var tokenOne = objOne.toLowerCase().replace(/[:,.]/g, '').split(' ');
-
-Index.prototype.createIndex = function(filepath) {
-  var invertedIndex = {};
-  for(var i in tokenZero) 
-    invertedIndex[tokenZero[i]] = [0];
-  for(var j in tokenOne) 
-    invertedIndex[tokenOne[j]] = [1];
-  return invertedIndex; 
+var Index = function(){
+  this.books = books;
 };
 
-var arrIndex = new Index();
-var getIndex = arrIndex.createIndex();
+Index.prototype.createIndex = function() {
+  var indexObj = {},
+  filePath = this.books;
+  for ( var i = 0; i < filePath.length; i++ ) {
+    for ( var j in filePath[i] ) {
 
-Index.prototype.searchIndex = function(terms){
-  if(tokenZero.indexOf(terms) >= 0 && tokenOne.indexOf(terms) >= 0) {
-    return [0, 1];
+      // tokenisation of json file contents
+      var arrToken = filePath[i][j].split(' ');
+      for ( var k = 0; k < arrToken.length; k++ ) {
+        var strToken = arrToken[k].toLowerCase().replace(/[:,.]/g, '');
+
+        // building the index from the tokenized words
+        if ( indexObj.hasOwnProperty(strToken) ) {
+          var objKey = indexObj[strToken];
+          if ( parseInt(i) ) {
+            var arrObj = parseInt(i);
+            if ( objKey.indexOf(arrObj) < 0 ) {
+              objKey.push(arrObj);
+              indexObj[strToken] = objKey;
+            }
+          }
+        } 
+        else {
+          indexObj[strToken] = [parseInt(i)];
+        }
+      }
+    }
   }
-  else if (tokenZero.indexOf(terms) >= 0 && tokenOne.indexOf(terms) <= 0) {
-    return [0];
+  return indexObj;
+};
+
+var indexChild = new Index();
+var getIndex = indexChild.createIndex();
+
+Index.prototype.searchIndex = function(terms) {
+  var searchArr = [];
+
+  // looping through the arguments to allow for multiple words search
+  for ( var i = 0; i < terms.length; i++ ) {
+    if ( getIndex.hasOwnProperty(terms[i]) ) {
+      searchArr.push(getIndex[terms[i]]);
+    } 
+    else {
+      searchArr.push('Word Not Found!');
+    }
   }
-  else if (tokenZero.indexOf(terms) <= 0 && tokenOne.indexOf(terms) >= 0) {
-    return [1];
-  }
-  else if (tokenZero.indexOf(terms) <= 0 && tokenOne.indexOf(terms) <= 0) {
-    return 'Not In Any Document';
-  }
+  return searchArr;
 };
