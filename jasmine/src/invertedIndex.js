83548484c10c122c2439bacd3623544dc3eb5147
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-09-28 11:04:56 +0100

    refactor index prototypes

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,9 +1,12 @@
-// get the json file contents
-var getBooks = function() {
+// index constructor
+var Index = function(){};
+
+// method to retrieve the json file contents
+Index.prototype.getBooks = function(filePath) {
   var booksJson;
   $.ajax({
     async: false,
-    url: 'books.json',
+    url: filePath,
     dataType: 'json',
     success: function(response) {
       booksJson = response;
@@ -11,22 +14,18 @@ var getBooks = function() {
   });
   return booksJson;
 };
-var books = getBooks();
 
-var Index = function(){
-  this.books = books;
-};
-
-Index.prototype.createIndex = function() {
+// method to create the index from the json file contents
+Index.prototype.createIndex = function(filePath) {
   var indexObj = {},
-  filePath = this.books;
-  for ( var i = 0; i < filePath.length; i++ ) {
-    for ( var j in filePath[i] ) {
+  booksArr = this.getBooks(filePath);
+  for ( var i = 0; i < booksArr.length; i++ ) {
+    for ( var j in booksArr[i] ) {
 
       // tokenisation of json file contents
-      var arrToken = filePath[i][j].split(' ');
+      var arrToken = booksArr[i][j].split(' ');
       for ( var k = 0; k < arrToken.length; k++ ) {
-        var strToken = arrToken[k].toLowerCase().replace(/[:,.]/g, '');
+        var strToken = arrToken[k].toLowerCase().replace(/[^\w\s]/g, '');
 
         // building the index from the tokenized words
         if ( indexObj.hasOwnProperty(strToken) ) {
@@ -48,9 +47,7 @@ Index.prototype.createIndex = function() {
   return indexObj;
 };
 
-var indexChild = new Index();
-var getIndex = indexChild.createIndex();
-
+// method to search the index for specific word(s)
 Index.prototype.searchIndex = function(terms) {
   var searchArr = [];
 
@@ -65,3 +62,7 @@ Index.prototype.searchIndex = function(terms) {
   }
   return searchArr;
 };
+
+// new instance of Index
+var indexChild = new Index();
+var getIndex = indexChild.createIndex('books.json');
