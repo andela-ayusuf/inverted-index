76232e797a2c8c2a1b211c2220330f3dff38e345
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-10-02 00:50:18 +0100

    refactor getIndex and searchIndex methods

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -19,20 +19,20 @@ Index.prototype.getBooks = function(filePath) {
 Index.prototype.createIndex = function(filePath) {
   var indexObj = {},
   booksArr = this.getBooks(filePath);
-  for ( var i = 0; i < booksArr.length; i++ ) {
-    for ( var j in booksArr[i] ) {
+  for (var i = 0; i < booksArr.length; i++) {
+    for (var j in booksArr[i]) {
 
       // tokenisation of json file contents
       var arrToken = booksArr[i][j].split(' ');
-      for ( var k = 0; k < arrToken.length; k++ ) {
+      for (var k = 0; k < arrToken.length; k++) {
         var strToken = arrToken[k].toLowerCase().replace(/[^\w\s]/g, '');
 
         // building the index from the tokenized words
-        if ( indexObj.hasOwnProperty(strToken) ) {
+        if (indexObj.hasOwnProperty(strToken)) {
           var objKey = indexObj[strToken];
-          if ( parseInt(i) ) {
+          if (parseInt(i)) {
             var arrObj = parseInt(i);
-            if ( objKey.indexOf(arrObj) < 0 ) {
+            if (objKey.indexOf(arrObj) < 0) {
               objKey.push(arrObj);
               indexObj[strToken] = objKey;
             }
@@ -44,18 +44,24 @@ Index.prototype.createIndex = function(filePath) {
       }
     }
   }
+  this.indexObj = indexObj; 
   return indexObj;
 };
 
-// method to search the index for specific word(s)
-Index.prototype.searchIndex = function(terms) {
-  var searchArr = [];
+// method to get the index of a word
+Index.prototype.getIndex = function (filePath) {
+  return this.createIndex(filePath); 
+};
 
-  // looping through the arguments to allow for multiple words search
-  for ( var i = 0; i < terms.length; i++ ) {
-    if ( getIndex.hasOwnProperty(terms[i]) ) {
-      searchArr.push(getIndex[terms[i]]);
-    } 
+// method to search the index for specific word(s)
+Index.prototype.searchIndex = function (terms) {
+  var searchArr = [];
+  for (var l in arguments) {
+    var params = arguments[l],
+        indices = this.indexObj;
+    if (indices.hasOwnProperty(params)) {
+      searchArr.push(indices[params]);
+    }
     else {
       searchArr.push('Word Not Found!');
     }
@@ -65,4 +71,3 @@ Index.prototype.searchIndex = function(terms) {
 
 // new instance of Index
 var indexChild = new Index();
-var getIndex = indexChild.createIndex('books.json');
