6d8542b22d2dd7bc82c0a1ff65d10e07a1ee2997
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-10-02 00:56:02 +0100

    refactor tests

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -15,40 +15,35 @@ describe('Read book data', function() {
 });
 
 describe('Populate Index', function() {
-  var getIndex = indexChild.createIndex('books.json'),
-  books = indexChild.getBooks('books.json');
+  var index = indexChild.createIndex('books.json');
 
   it('should verify that the index is created once the JSON file has been read', function() {
-    expect(getIndex).toBeDefined();
+    expect(index).toBeDefined();
   });
 
   it('should ensure that index is correct', function() {
-    expect(getIndex.rabbit).toEqual([0]);
+    expect(index.rabbit).toEqual([0]);
   });
 
   it('should ensure that index is correct', function() {
-    expect(getIndex.man).toEqual([1]);
-  });
-
-  it('should maps the string keys to the correct objects in the JSON array', function() {
-    expect(books[0].title).toEqual('Alice in Wonderland');
+    expect(index.man).toEqual([1]);
   });
 });
 
 describe('Search Index', function() {
-  it('should return an array of indices of the correct objects that contain the searched words', function() {
-    expect(Index.prototype.searchIndex(['and'])).toEqual([ [ 0, 1 ] ]);
+  it('should return an array of index of the correct objects that contain the searched word', function() {
+    expect(indexChild.searchIndex('and')).toEqual([[0, 1]]);
   });
 
   it('should return an array of the index of the correct object that contain the searched word', function() {
-    expect(Index.prototype.searchIndex(['alice'])).toEqual([ [ 0 ] ]);
+    expect(indexChild.searchIndex('alice')).toEqual([[0]]);
   });
 
   it('should return an appropriate message if searched word is not in the book', function() {
-    expect(Index.prototype.searchIndex(['andela'])).toEqual([ 'Word Not Found!' ]);
+    expect(indexChild.searchIndex('andela')).toEqual(['Word Not Found!']);
   });
 
   it('should take an array of search terms and return the correct indices of each word', function() {
-    expect(Index.prototype.searchIndex(['hole', 'andela', 'of'])).toEqual([ [ 0 ], 'Word Not Found!', [ 0, 1 ] ]);
+    expect(indexChild.searchIndex('hole', 'andela', 'of')).toEqual([[0], 'Word Not Found!', [0, 1]]);
   });
 });
